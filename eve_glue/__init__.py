ebdecc778ef9cf4c81599eb19187c5ae8f944c03
Lou Xun <louxun@ccpgames.com>
2017-04-25 13:53:34 +0000

    Add resolver for order_state enum

--- eve_glue/__init__.py
+++ eve_glue/__init__.py
@@ -1,2 +1,3 @@
 from eve_glue.location_type import resolve_location_type_enum  # noqa
 from eve_glue.location_flag import resolve_location_flag_enum  # noqa
+from eve_glue.order_state import resolve_order_state_enum  # noqa
---
+++ eve_glue/order_state.py
@@ -0,0 +1,22 @@
+ORDER_STATE_TO_ENUM = {
+    0: "open",
+    1: "closed",
+    2: "expired",
+    3: "cancelled",
+    4: "pending",
+    5: "character_deleted"
+}
+
+def resolve_order_state_enum(order_state_id):
+    """
+    order_state:
+        type: string
+        enum:
+          - open
+          - closed
+          - expired
+          - cancelled
+          - pending
+          - character_deleted
+    """
+    return ORDER_STATE_TO_ENUM[order_state_id]
