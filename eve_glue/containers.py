801b50fcb3900b9da4a1894ac957728cc3954734
CCP Zoetrope <eric@ccpgames.com>
2017-10-13 09:35:07 +0000

    Snake cased ALSC enums

--- eve_glue/containers.py
+++ eve_glue/containers.py
@@ -4,20 +4,20 @@ import enum
 class ActionsEnumV1(enum.Enum):
     """Maps ALSC Actions to IDs. Taken from the invALSCActions db table."""
 
-    Assemble = 1
-    Repackage = 2
-    SetName = 3
-    Move = 4
-    SetPassword = 5
-    Add = 6
+    assemble = 1
+    repackage = 2
+    set_name = 3
+    move = 4
+    set_password = 5
+    add = 6
     Lock = 7
-    Unlock = 8
-    EnterPassword = 9
-    Configure = 10
+    unlock = 8
+    enter_password = 9
+    configure = 10
 
 
 class PasswordTypeEnumV1(enum.Enum):
     """Maps ALSC password types to IDs."""
 
-    General = 1
-    Config = 2
+    general = 1
+    config = 2
