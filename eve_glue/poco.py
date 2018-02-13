25ea70eda1ad94ccdf07bc52a576100934f4da28
CCP Zoetrope <eric@ccpgames.com>
2018-02-13 09:48:52 +0000

    Add POCO state enum

---
+++ eve_glue/poco.py
@@ -0,0 +1,19 @@
+"""Helpers for player owned customs offices (poco)."""
+import enum
+
+
+class StructureStateEnumV1(enum.Enum):
+    """Enum for poco states."""
+
+    unknown = 0
+    unanchored = 1
+    anchoring = 2
+    online_deprecated = 100
+    fitting_invulnerable = 101
+    onlining_vulnerable = 102
+    shield_vulnerable = 110
+    armor_reinforce = 111
+    armor_vulnerable = 112
+    hull_reinforce = 113
+    hull_vulnerable = 114
+    anchor_vulnerable = 115
