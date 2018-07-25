f2f41da03acd7d64443ddceef4bb4f2c0e4406ce
CCP Zoetrope <eric@ccpgames.com>
2018-07-25 14:20:01 +0000

    Fix pylint error

--- eve_glue/location_type.py
+++ eve_glue/location_type.py
@@ -6,6 +6,6 @@ def resolve_location_type_enum(location_type_id):
 
     if location_type_id <= 10000:
         return "solar_system"
-    if (location_type_id >= 60000000) and (location_type_id < 64000000):
+    if 60000000 <= location_type_id < 64000000:
         return "station"
     return "other"
