cb1e162098c602c0cae76c8aa19e073e7abb1b00
Mateusz Krasucki <mateusz@ccpgames.com>
2019-11-12 16:55:03 +0000

    Revert "ADD converting proto standing value msg. to float"
    This reverts commit 98289618875d793eb506196fa6318e098260f59b.
    
    - We cannot use internal libs in eve-glue.

--- eve_glue/standing.py
+++
@@ -1,7 +0,0 @@
-"""Helpers for evaluating standing."""
-
-
-def read_standing_from_proto(standing_proto):
-    """Convert protobuf standing value message to float."""
-
-    return standing_proto.units + (standing_proto.nanos * (10**-9))
