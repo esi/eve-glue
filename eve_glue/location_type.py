b0d12fc0190c2b5f8c5a773e493cbdfce375da7f
gitAskur <unnar@ccpgames.com>
2019-12-03 14:09:21 +0000

    add item range and amend comment and argument name

--- eve_glue/location_type.py
+++ eve_glue/location_type.py
@@ -1,11 +1,14 @@
 """Helpers for resolving the location type by ID."""
 
 
-def resolve_location_type_enum(location_type_id):
-    """Resolve the location type ID to name."""
+def resolve_location_type_enum(location_id):
+    """Resolve the location itemID ID to a type name."""
 
-    if 30000000 <= location_type_id <= 39999999:
+    if 30000000 <= location_id <= 39999999:
         return "solar_system"
-    if 60000000 <= location_type_id < 64000000:
+    if 60000000 <= location_id < 64000000:
         return "station"
+    if location_id >= 100000000:
+        return "item"
+
     return "other"
